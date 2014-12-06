2cc24f2956f71d5b7c2fa9df4800db01bf051b30
Taufiq Hoven <taufiq.hoven@gmail.com>
2014-12-06 22:50:24 +1000

    Created parent player effect class

---
+++ src/main/java/net/buddat/ludumdare/ld31/render/PlayerEffect.java
@@ -0,0 +1,43 @@
+package net.buddat.ludumdare.ld31.render;
+
+/**
+ * Visual effect on the player
+ */
+public abstract class PlayerEffect {
+	private int x;
+	private int y;
+	private int durationCompleted = 0;
+
+	public PlayerEffect(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * @return X centre of the effect
+	 */
+	protected int getX() {
+		return x;
+	}
+
+	/**
+	 * @return Y centre of the effect
+	 */
+	protected int getY() {
+		return y;
+	}
+
+	public void update(int delta) {
+		durationCompleted += delta;
+	}
+
+	public int getDurationCompleted() {
+		return durationCompleted;
+	}
+
+	public abstract int getDuration();
+
+	public boolean hasExpired() {
+		return durationCompleted >= getDuration();
+	}
+}
