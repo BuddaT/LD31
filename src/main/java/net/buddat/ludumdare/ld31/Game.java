ca56a6e1107985490df526bef68dfbae56edb605
Taufiq Hoven <taufiq.hoven@gmail.com>
2014-12-06 14:06:02 +1000

    Moved java files to maven default

--- src/main/java/net/buddat/ludumdare/ld31/Game.java
+++ src/main/java/net/buddat/ludumdare/ld31/Game.java
@@ -30,7 +30,6 @@ public class Game extends BasicGame {
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
 		// TODO Auto-generated method stub
-
 	}
 
 	public static void main(String[] args) {
